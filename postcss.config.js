ff707ee01617ca3200d274600d63ba63222dc733
napee19 <napee1904@gmail.com>
2022-08-27 16:25:07 -0700

    create app & install deps

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{jx,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
