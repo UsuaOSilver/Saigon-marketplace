47959d4cf8c29a88b1ff4ef30aa6fd5d72b883b7
napee19 <napee1904@gmail.com>
2022-08-29 14:38:26 -0700

    Add functions outline

--- contracts/SaigonMarket.sol
+++ contracts/SaigonMarket.sol
@@ -4,31 +4,58 @@ pragma solidity ^0.8.9;
 // Import this file to use console.log
 import "hardhat/console.sol";
 
-contract SaigonMarket {
-    uint public unlockTime;
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SaigonMarket is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemdSold;
+    
+    uint256 public listingPrice = 0.25 ether;
     address payable public owner;
-
-    event Withdrawal(uint amount, uint when);
-
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+    
+    mapping(uint256 => MarketItem) private idToMarketItem;
+    
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
     }
-
-    function withdraw() public {
-        // Uncomment this line to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
-    }
+    
+    event MarketitemCreated();
+    
+    constructor() ERC721("Metaverse Token", "METT") {}
+    
+    /** Updates the listing price of the contract */
+    function updateListingPrice() public view returns (uint256) {}
+    
+    /** Returns the listing price of the contract */
+    function getListingPrice() public view returns (uint256) {}
+    
+    /** Mint a token and lists it in the marketplace */
+    function createToken(string memory tokenURI, uint256 price) public payable returns (uint) {}
+    
+    function createMarketItem(uint256 tokenId, uint256 price) private {}
+    
+    /** Allow someone to resell a token they have purchased */
+    function resellToken(uint256 tokenId, uint256 price) public payable {}
+    
+    /** Create the sale of a marketplace item.
+      * Trasnfer ownership of the item, as well as funds between parties 
+      */
+    function createMarketSale(uint256 tokenId) public payable {}
+    
+    /** Returns all unsold market items */
+    function fetchMarketItems() public view returns (MarketItem[] memory) {}
+    
+    /** Returns only the items that the user has purchased */
+    function fetchMyNFTs() public view returns (MarketItem[] memory) {}
+    
+    /** Returns only item a user has listed */
+    function fetchItemsListed() public view returns (MarketItem[] memory) {}
+    
 }
