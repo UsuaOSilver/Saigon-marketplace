dbc595deb002fa0cf822b15cc985b58b13fcd281
napee19 <napee1904@gmail.com>
2022-08-29 17:19:37 -0700

    Build nav bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
 import '../styles/globals.css'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p></p>
+        <div>
+          <Link>
+            <a></a>
+          </Link>
+          <Link>
+            <a></a>
+          </Link>
+          <Link>
+            <a></a>
+          </Link>
+          <Link>
+            <a></a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
