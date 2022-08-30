1df09d188db855681aff9492e285d5b00051bb5f
napee19 <napee1904@gmail.com>
2022-08-29 22:33:51 -0700

    update tailwind config

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,4 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{jx,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
