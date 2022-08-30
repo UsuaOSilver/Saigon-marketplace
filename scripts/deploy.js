8ff3e29428d886accd11c95a9d8f5b02045dc669
napee19 <napee1904@gmail.com>
2022-08-29 22:10:09 -0700

    Fix typo & deploy to localhost

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,5 @@
 const hre = require("hardhat");
-const fs = reuire('fs');
+const fs = require('fs');
 
 async function main() {
   const SaigonMarket = await hre.ethers.getContractFactory("SaigonMarket");
