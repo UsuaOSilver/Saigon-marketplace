d62b5170c90facc45ecf393c8fb0ec3240b234f7
napee19 <napee1904@gmail.com>
2022-08-29 20:54:53 -0700

    Add loadNFTs() and listNFTs() functions

--- pages/my-nfts.js
+++ pages/my-nfts.js
@@ -17,9 +17,39 @@ export default function MyAssets() {
         loadNFTs()
     }, [])
     
-    async function laodNFTs() {}
+    async function loadNFTs() {
+        const web3Modal = new Web3Modal({
+            network: "mainet",
+            cacheProvider: true,
+        })
+        const connection = await web3Modal.connect()
+        const provider = await ethers.providers.Web3Provider(connection)
+        const signer = provider.getSigner()
+            
+        const marketplaceContract = new ethers.Contract(marketplaceAddress, SaigonMarket.abi, signer)
+        const data = await marketplaceContract.fetchMyNFTs()
+        
+        const items = await Promise.all(data.map(async i => {
+            const tokenURI = await marketplaceContract.tokenURI(i.tokenId)
+            const meta = await axios.get(tokenURI)
+            let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+            let item = {
+                price,
+                tokenId: i.tokenId.toNumber(),
+                seller: i.seller,
+                owner: i.owner,
+                image: meta.data.image,
+                tokenURI
+            }
+            return item
+        }))
+        setNfts(items)
+        setLoadingState('loaded')
+    }
     
-    function listNFT(nft) {}
+    function listNFT(nft) {
+        router.push(`/resell-nft?id=${nft.tokenId}&tokenURI=${nft.tokenURI}`)
+    }
     
     if(loadingState === 'loaded' && !nfts.length) return (<h1 className='py-10 px-20 text-3xl'>No NFTs owned</h1>)
     
