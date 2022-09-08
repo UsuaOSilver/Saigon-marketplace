15cf2d1a8a409d9ccfd74bf331e7a3fd94e0ac3f
napee19 <napee1904@gmail.com>
2022-09-08 16:12:31 -0700

    Create NftFactory.sol

---
+++ contracts/NftFactory.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.7;
+
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+/** 1. Compatible with ERC-721 standard */
+contract FstNFT is ERC721Enumerable, Ownable {
+    using Counters for Counters.Counter;
+    
+    Counters.Counter private _nftIds;
+    
+    constructor() ERC721("NFTToken", "NFTT") {}
+    
+    function mintNft(string memory nftIds) public returns (uint256) {
+        _nftIds.increment();
+        
+        uint256 newNftId = _nftIds.current();
+        _safeMint(msg.sender, newNftId);
+        _setTokenURI(newNftId, tokenURI);
+        
+        return newNftId;
+    }    
+}
+
